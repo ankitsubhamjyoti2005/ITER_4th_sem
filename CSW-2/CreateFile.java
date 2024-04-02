3f98ec91a2def8f4dddc8db8dcb3d31001f9f817
iter <iter@soa.ac.in>
2024-04-02 13:30:13 +0530

    modified:   CSW-2/CreateFile.java

--- CSW-2/CreateFile.java
+++ CSW-2/CreateFile.java
@@ -3,16 +3,21 @@ import java.io.IOException;
 
 public class CreateFile {
   public static void main(String[] args) {
-    try {
-      File myObj = new File("name.txt");
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-      } else {
-        System.out.println("File already exists.");
-      }
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
+    // try {
+    //   File myObj = new File("name.txt");
+    //   if (myObj.createNewFile()) {
+    //     System.out.println("File created: " + myObj.getName());
+    //   } else {
+    //     System.out.println("File already exists.");
+    //   }
+    // } catch (IOException e) {
+    //   System.out.println("An error occurred.");
+    //   e.printStackTrace();
+    // }
+    int a[]={1,2,3},b[]={4,5,6},c[]=new int[3];
+        for (int i = 0; i < 3; i++) {
+            c[i]=a[i]*b[i];
+            System.out.println("At "+(i+1)+" position: "+c[i]);
+        }
   }
 }
